59062f4752cb9d12b2221ec2460cde5ed4ee02da
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-08-30 17:09:54 +0600

    Deshboard Layout setup for Deshboard UI Design

---
+++ Areas/User/Controllers/DeshboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrganicOption.Areas.User.Controllers
+{
+    [Area("User")]
+    public class DeshboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
